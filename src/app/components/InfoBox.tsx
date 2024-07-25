d6bb26d40137fa618235cfb051a1061d36d9a931
KKKHun2 <1224kim2son@naver.com>
2024-07-25 21:46:13 +0900

    ✨반응형 화면 추가 간편하게 보이게 수정

--- src/app/components/InfoBox.tsx
+++ src/app/components/InfoBox.tsx
@@ -12,18 +12,17 @@ interface InfoBoxProps {
 
 const InfoBox: React.FC<InfoBoxProps> = ({ brand, model, year, mileage, nickname }) => {
   return (
-    <div className="bg-gray-800 text-white pt-8 p-10 rounded-lg shadow-lg">
+    <div className="bg-gray-800 text-white pt-8 p-10 rounded-lg shadow-lg w-full max-w-full sm:p-8">
       <h2 className="text-xl font-bold">Car Information</h2>
-      <div className='mt-6 font-medium text-lg'>
+      <div className="mt-6 font-medium text-lg sm:mt-2 sm:text-base">
         <p><strong>브랜드:</strong> {brand}</p>
       </div>
-      <div className='flex gap-10 mt-1 font-medium text-xl'>
+      <div className="flex flex-col gap-3 mt-2 font-medium text-xl sm:flex-row sm:gap-10 sm:mt-4 pb-2">
         <p><strong>모델명:</strong> {model}</p>
         <p><strong>연식:</strong> {year}</p>
         <p><strong>주행거리:</strong> {mileage} K/m</p>
         <p><strong>별명:</strong> {nickname}</p>
       </div>
-
     </div>
   );
 }
