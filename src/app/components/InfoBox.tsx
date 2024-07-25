4f2c90af29731556396e99b0058184b375d8be17
KKKHun2 <1224kim2son@naver.com>
2024-07-25 21:50:39 +0900

    ✨ 간격 , 크기수정

--- src/app/components/InfoBox.tsx
+++ src/app/components/InfoBox.tsx
@@ -13,9 +13,9 @@ interface InfoBoxProps {
 const InfoBox: React.FC<InfoBoxProps> = ({ brand, model, year, mileage, nickname }) => {
   return (
     <div className="bg-gray-800 text-white pt-8 p-10 rounded-lg shadow-lg w-full max-w-full sm:p-8">
-      <h2 className="text-xl font-bold">Car Information</h2>
-      <div className="mt-6 font-medium text-lg sm:mt-2 sm:text-base">
-        <p><strong>브랜드:</strong> {brand}</p>
+      <h2 className="text-2xl font-bold">Car Information</h2>
+      <div className="mt-6 font-medium  sm:mt-8 sm:text-base">
+        <p className='text-xl'><strong>브랜드:</strong> {brand}</p>
       </div>
       <div className="flex flex-col gap-3 mt-2 font-medium text-xl sm:flex-row sm:gap-10 sm:mt-4 pb-2">
         <p><strong>모델명:</strong> {model}</p>
