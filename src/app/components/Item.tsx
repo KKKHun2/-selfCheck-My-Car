9889f35d933d8cbb69c309e6ce9b27ada70bf1c1
KKKHun2 <1224kim2son@naver.com>
2024-07-27 22:15:13 +0900

    ✨ 아이템 글씨 수정

--- src/app/components/Item.tsx
+++ src/app/components/Item.tsx
@@ -12,9 +12,9 @@ const Item: React.FC<ItemProps> = ({ name, count, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="w-40 h-40 bg-gray-200 flex flex-col items-center justify-center m-2 cursor-pointer hover:bg-gray-300 rounded-lg"
+      className="w-40 h-40 bg-gray-200 flex flex-col items-center justify-center m-2 p-2 cursor-pointer hover:bg-gray-300 rounded-lg"
     >
-      <p className="text-lg font-bold">{name}</p>
+      <p className="text-sm font-bold">{name}</p>
       <p className="text-sm text-gray-700">{count} 회</p>
     </div>
   );
