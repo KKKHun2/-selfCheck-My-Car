0e57a54ed34d90f2a08bf82c3ee655d543011742
KKKHun2 <1224kim2son@naver.com>
2024-07-28 22:51:43 +0900

    ✨ 팝업 높이수정

--- src/app/components/Popup.tsx
+++ src/app/components/Popup.tsx
@@ -52,7 +52,7 @@ const Popup: React.FC<PopupProps> = ({ item, onClose, onSave, onDelete }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center  bg-black bg-opacity-50">
-      <div className="relative bg-white p-6 rounded-lg shadow-lg w-full max-w-md max-h-[85vh] overflow-auto ">
+      <div className="relative bg-white p-6 rounded-lg shadow-lg w-full max-w-md max-h-[90vh] overflow-auto ">
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
@@ -68,7 +68,7 @@ const Popup: React.FC<PopupProps> = ({ item, onClose, onSave, onDelete }) => {
             <TrashIcon className="h-6 w-6" />
           </button>
         </div>
-        <div className="mb-6">
+        <div className="mb-2">
           <h3 className="text-xl mb-2">점검 기록</h3>
           <ul>
             {sortedRecords.map((record, index) => (
