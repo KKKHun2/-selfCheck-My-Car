3ee7c6be5cb82c2d7f2de9ff08b428ac4fbaafd0
KKKHun2 <1224kim2son@naver.com>
2024-07-31 23:39:21 +0900

    ✨ 팝업 가로90vw 누락추가

--- src/app/components/AddItemPopup.tsx
+++ src/app/components/AddItemPopup.tsx
@@ -21,7 +21,7 @@ const AddItemPopup: React.FC<AddItemPopupProps> = ({ onClose, onSave }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
+      <div className="relative bg-white p-6 rounded-lg shadow-lg w-[90vw] max-w-md">
         <button
           onClick={onClose}
           className="absolute top-5 right-6 text-gray-500 hover:text-gray-700"
