f53a1042b41f827e5e047e4461995e8483f1f480
KKKHun2 <1224kim2son@naver.com>
2024-07-31 23:39:36 +0900

    ✨ 인포박스 폰트 크기 다시 수정

--- src/app/components/InfoBox.tsx
+++ src/app/components/InfoBox.tsx
@@ -15,13 +15,13 @@ const InfoBox: React.FC<InfoBoxProps> = ({ brand, model, year, mileage, nickname
     <div className="bg-gray-800 text-white pt-4 p-6 rounded-lg shadow-lg w-full max-w-full sm:p-4">
       <h2 className="text-2xl font-bold">Car Maintenance Records</h2>
       <div className="mt-4 font-medium sm:mt-6 sm:text-base">
-        <p><strong className='text-xl'>브랜드:</strong> <span className='text-2xl'>{brand}</span></p>
+        <p><strong className='text-lg'>브랜드:</strong> <span className='text-lg'>{brand}</span></p>
       </div>
       <div className="flex flex-col gap-3 mt-2 font-medium text-lg sm:flex-row sm:gap-10 sm:mt-4 pb-2">
-        <div><strong className='text-xl'>모델명:</strong> <span className='text-2xl'>{model}</span></div>
-        <div><strong className='text-xl'>연식:</strong> <span className='text-2xl'>{year} 년</span></div>
-        <div><strong className='text-xl'>주행거리:</strong> <span className='text-2xl'>{mileage} K/m</span></div>
-        <div><strong className='text-xl'>별명:</strong> <span className='text-2xl'>{nickname}</span></div>
+        <div><strong className='text-lg'>모델명:</strong> <span className='text-lg'>{model}</span></div>
+        <div><strong className='text-lg'>연식:</strong> <span className='text-lg'>{year} 년</span></div>
+        <div><strong className='text-lg'>주행거리:</strong> <span className='text-lg'>{mileage} K/m</span></div>
+        <div><strong className='text-lg'>별명:</strong> <span className='text-lg'>{nickname}</span></div>
       </div>
     </div>
   );
